b17d75225d3e88f4d13862ac01ba4a60b23ed2d0
Kevin Washington <washintgonkevin703@gmail.com>
2026-05-19 22:50:05 -0300

    feat: atualiza layout base da aplicação

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,11 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Accessibility, LucideAngularModule, ShieldCheck, University } from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
   protected readonly currentYear = new Date().getFullYear();
+
+  protected readonly icons = {
+    accessibility: Accessibility,
+    shield: ShieldCheck,
+    university: University,
+  };
 }
