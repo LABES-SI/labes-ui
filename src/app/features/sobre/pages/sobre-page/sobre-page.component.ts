474a28ec72cb6034d4955e0f16c861ee6890d882
Kevin Washington <washintgonkevin703@gmail.com>
2026-05-20 20:10:33 -0300

    styles(sobre): corrige estilo para seguir padrão do projeto

--- src/app/features/sobre/pages/sobre-page/sobre-page.component.ts
+++ src/app/features/sobre/pages/sobre-page/sobre-page.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { SobreHeroComponent } from '../../components/sobre-hero/sobre-hero.component';
 import { SobreOQueEComponent } from '../../components/sobre-o-que-e/sobre-o-que-e.component';
+import { FontesDadosComponent } from '../../components/fontes-dados/fontes-dados.component';
 
 @Component({
   selector: 'app-sobre-page',
   standalone: true,
-  imports: [SobreHeroComponent, SobreOQueEComponent],
+  imports: [SobreHeroComponent, SobreOQueEComponent, FontesDadosComponent],
   templateUrl: './sobre-page.component.html',
   styleUrl: './sobre-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
